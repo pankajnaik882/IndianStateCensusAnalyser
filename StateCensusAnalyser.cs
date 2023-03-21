ec05f25cb82589bb3b20d66af182a7e2799927b4
“pankajnaik882” <pankaj080519999@gmail.com>
2023-03-21 15:08:18 +0530

    Add : Custom Exception For Incorrect Typing

---
+++ StateCensusAnalyser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyserProblem
+{
+    public class StateCensusAnalyser
+    {
+        private readonly string csv_file = "C:\\Users\\USER\\source\\repos\\IndianStateCensusAnalyserProblem\\IndianStateCensusAnalyserProblem\\state_census.csv";
+
+        public StateCensusAnalyser(string csv_file)
+        {
+            this.csv_file = csv_file;
+        }
+
+        public int GetRecordCount()
+        {
+            var csv_data = new CSVStateCensus(csv_file).GetCensusData();
+            return csv_data.Count;
+        }
+    }
+}
---
+++ StateCensusData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyserProblem
+{
+    public class StateCensusData
+    {
+        public string StateName { get; set; }
+        public int Population { get; set; }
+        public int AreaInSquareKm { get; set; }
+        public int DensityPerSquareKm { get; set; }
+    }
+}
