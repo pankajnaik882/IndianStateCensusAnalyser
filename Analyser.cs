4985451edd946fd5dc8ac3bbcb281b74e3927cb7
“pankajnaik882” <pankaj080519999@gmail.com>
2023-03-21 14:40:07 +0530

    Add : Creating Custom Exception

--- Analyser.cs
+++ Analyser.cs
@@ -7,44 +7,16 @@ using System.IO;
 
 namespace IndianStateCensusAnalyserProblem
 {
-
     public class CSVStateCensus
     {
-        private readonly string csv_file = "C:\\Users\\USER\\source\\repos\\IndianStateCensusAnalyserProblem\\IndianStateCensusAnalyserProblem\\state_census.csv";
+        private readonly string csv_file = "C:\\Users\\USER\\source\\repos\\IndianStateCensusAnalyserProblem\\IndianStateCensusAnalyserProblem\\StateCensusException.cs";
 
         public CSVStateCensus(string csv_file)
         {
             this.csv_file = csv_file;
         }
 
-        public int GetRecordCount()
-        {
-            try
-            {
-                var csv_data = File.ReadAllLines("C:\\Users\\USER\\source\\repos\\IndianStateCensusAnalyserProblem\\IndianStateCensusAnalyserProblem\\state_census.csv").Skip(1);
-                return csv_data.Count();
-            }
-            catch (FileNotFoundException)
-            {
-                throw new ArgumentException("File not found.");
-            }
-        }
-    }
-
-    public class StateCensusAnalyser
-    {
-        private readonly string csv_file = "C:\\Users\\USER\\source\\repos\\IndianStateCensusAnalyserProblem\\IndianStateCensusAnalyserProblem\\state_census.csv";
-
-        public StateCensusAnalyser(string csv_file)
-        {
-            this.csv_file = csv_file;
-        }
-
-        public int GetRecordCount()
-        {
-            var csv_data = new CSVStateCensus("C:\\Users\\USER\\source\\repos\\IndianStateCensusAnalyserProblem\\IndianStateCensusAnalyserProblem\\state_census.csv").GetRecordCount();
-            return csv_data;
-        }
+      
     }
 
 }
