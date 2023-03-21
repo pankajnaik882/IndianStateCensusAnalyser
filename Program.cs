2f9facb7e06ec40d09faa1d6dce9fec8289e23a6
“pankajnaik882” <pankaj080519999@gmail.com>
2023-03-21 14:38:09 +0530

    Add : Added Program.cs File

--- Program.cs
+++ Program.cs
@@ -7,6 +7,16 @@ namespace IndianStateCensusAnalyserDemo
         public static void Main(string[] args)
         {
             
+                try
+                {
+                    var csv_data = File.ReadAllLines("C:\\Users\\USER\\source\\repos\\IndianStateCensusAnalyserProblem\\IndianStateCensusAnalyserProblem\\state_census.csv").Skip(1);
+                    Console.WriteLine( csv_data.Count());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine( ex.Message);
+                }
+            
 
         }
     }
