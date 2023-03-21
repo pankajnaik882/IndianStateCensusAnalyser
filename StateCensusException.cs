9e8f48cde7782ea41f1282c2425aa5c1992f3efb
“pankajnaik882” <pankaj080519999@gmail.com>
2023-03-21 14:41:50 +0530

    Add : Added StateCensusException.cs File

---
+++ StateCensusException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyserProblem
+{
+    public class StateCensusException : Exception
+    {
+        public StateCensusException(string message) : base(message){ }
+    }
+}
