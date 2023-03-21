a3b957b616d93171356bed5175838ce37e910e96
“pankajnaik882” <pankaj080519999@gmail.com>
2023-03-21 13:35:26 +0530

    Add : Ability to Load CSV File

---
+++ Analyser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IndianStateCensusAnalyserProblem
+{
+    public class CSVStateCensus : IEnumerable<string[]>
+    {
+        private readonly string _filePath;
+
+        public CSVStateCensus(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public IEnumerator<string[]> GetEnumerator()
+        {
+            using (var reader = new StreamReader("C:\\Users\\USER\\source\\repos\\IndianStateCensusAnalyserProblem\\IndianStateCensusAnalyserProblem\\state_census.csv"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    yield return line.Split(',');
+                }
+            }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class StateCensusAnalyser
+    {
+        private readonly string _filePath;
+
+        public StateCensusAnalyser(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public int GetNumberOfRows()
+        {
+            var rows = new CSVStateCensus(_filePath);
+            int count = 0;
+
+            foreach (var row in rows)
+            {
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
